b5ac03ad0dd400ea219ac69d3975b854afdacbae
kindlove <skkkms@naver.com>
2025-04-23 16:12:04 +0900

    Commit git-test-b #3

--- vue.config.js
+++ vue.config.js
@@ -10,7 +10,7 @@ const wasEndPoint = require('./src/config/wasEndPoint.json')
 module.exports = {
 
   productionSourceMap: true,
-
+  
   // runtimeCompiler: true,
   // Setup Alias
   configureWebpack: uidev_tools.getConfigureWebpack(process.env.IDE_PAGE),
